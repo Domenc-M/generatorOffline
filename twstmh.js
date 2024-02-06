68b633db1ffccd2d58f0c07356118e6ddce581b9
Maxime <malryck33@gmail.com>
2024-02-06 17:49:23 +0100

    initialization

---
+++ twstmh.js
@@ -0,0 +1,33 @@
+typeList = [
+    {
+        typeName : "Arme à une main",
+        typeDamage : "1",
+        typeRange : "Mêlée",
+        typeVariations: ["Epée", "Sabre", "Masse", "Hache", "Dague", "Rapière", "Katana"],
+        tagsList: [
+            {}
+        ]
+    },
+    {
+        typeName : "Arme à deux mains",
+        typeDamage : "3",
+        typeRange : "Mêlée",
+        typeVariations: ["Epée à deux mains", "Odachi", "Faux", "Lance", "Trident", "Glaive", "Marteau à deux mains", "Morgensterg", "Hache à deux mains"]
+    },
+    {
+        typeName : "Arme à deux mains",
+        typeDamage : "3",
+        typeRange : "Mêlée",
+        typeVariations: ["Epée à deux mains", "Odachi", "Faux", "Lance", "Trident", "Glaive", "Marteau à deux mains", "Morgensterg", "Hache à deux mains"]
+    }
+]
+
+generalTagsList = [
+    {
+        TagName: ""
+    }
+]
+
+const generateWeapon = (type = null) => {
+
+}
